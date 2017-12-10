69aaf35a7df766a943a0cf22cdc0df28249e8847
Hannah Zhou <Hannah@Williams-MacBook-Air-2.local>
2017-12-10 14:08:19 -0800

    Added methods to TimeoutProcedureBase

--- src/com/palyrobotics/frc2017/vision/TimeoutProcedureBase.java
+++ src/com/palyrobotics/frc2017/vision/TimeoutProcedureBase.java
@@ -1,11 +1,32 @@
 package com.palyrobotics.frc2017.vision;
 
 public abstract class TimeoutProcedureBase {
+	public double baseValue; 
+	public double scalarValue; 
+	public TimeoutProcedureBase(double baseValue, double scalarValue){
+		this.baseValue = baseValue; 
+		this.scalarValue = scalarValue; 
+	}
 	public void success(){
 		
 	}
 	public void failure(){
 		
+	}
+	public boolean didSucceed(){
+		return true; 
+	}
+	public double doWait(){
+		return 0; 
+	}
+	public boolean succeed(){
+		return true; 
+	}
+	public double getDuration(){
+		return 0; 
+	}
+	protected double getBaseValue(){
+		return 0; 
 	}
 	public static int numFailures; 
 }
